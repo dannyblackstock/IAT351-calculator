56ecf71d926d3d683763d7182e306fcffa0b7654
Danny Blackstock <dannyblackstock@gmail.com>
2014-09-25 21:59:48 -0700

    Change to gridbag layout.

--- IAT351 Calculator/src/Calculator.java
+++ IAT351 Calculator/src/Calculator.java
@@ -1,7 +1,8 @@
 // Calculator GUI 	
 
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -17,7 +18,7 @@ public class Calculator {
 	private static final int WINDOW_HEIGHT = 275; // pixels
 	private static final int TEXT_WIDTH = 20; // characters
 
-	private static final FlowLayout LAYOUT_STYLE = new FlowLayout();
+	// private static final FlowLayout LAYOUT_STYLE = new FlowLayout();
 
 	// window for GUI
 	private JFrame window = new JFrame("Awesome Calculator");
@@ -44,13 +45,8 @@ public class Calculator {
 
 		window.getContentPane().add(panel);
 
-		// humidexText.setEditable(false);
-		// humidexText.setBackground(Color.WHITE);
-
-		// register event listener
-		// runButton.addActionListener(this);
-		// inputField.addActionListener(this);
-		// humText.addActionListener(this);
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 
 		// set up button arrays
 		numberButtons = new JButton[11];
@@ -58,15 +54,15 @@ public class Calculator {
 
 		// define buttons
 		numberButtons[0] = new JButton("0");
-		numberButtons[1] = new JButton("1");
-		numberButtons[2] = new JButton("2");
-		numberButtons[3] = new JButton("3");
+		numberButtons[1] = new JButton("7");
+		numberButtons[2] = new JButton("8");
+		numberButtons[3] = new JButton("9");
 		numberButtons[4] = new JButton("4");
 		numberButtons[5] = new JButton("5");
 		numberButtons[6] = new JButton("6");
-		numberButtons[7] = new JButton("7");
-		numberButtons[8] = new JButton("8");
-		numberButtons[9] = new JButton("9");
+		numberButtons[7] = new JButton("1");
+		numberButtons[8] = new JButton("2");
+		numberButtons[9] = new JButton("3");
 		numberButtons[10] = new JButton(".");
 
 		operatorButtons[0] = new JButton("+");
@@ -74,19 +70,27 @@ public class Calculator {
 		operatorButtons[2] = new JButton("*");
 		operatorButtons[3] = new JButton("/");
 		operatorButtons[4] = new JButton("%");
-		operatorButtons[5] = new JButton("=");
-		operatorButtons[6] = new JButton("C");
+		operatorButtons[5] = new JButton("C");
+		operatorButtons[6] = new JButton("=");
 
 		numberButtonColor = new Color(255, 0, 0); // red
 		operatorButtonColor = new Color(0, 0, 255); // blue
 
 		// arrange components in GUI
 		// Container c = window.getContentPane();
-		panel.setLayout(LAYOUT_STYLE);
-		panel.setLayout(LAYOUT_STYLE);
+		// panel.setLayout(LAYOUT_STYLE);
+		// panel.setLayout(LAYOUT_STYLE);
+		panel.setLayout(new GridBagLayout());
 
 		// add input field
-		panel.add(inputField);
+		c.gridx = 0;
+		c.gridy = 0;
+		c.fill = GridBagConstraints.BOTH;
+		c.gridheight = 1;
+		c.gridwidth = 5;
+		c.weightx = 1;
+		c.weighty = 1;
+		panel.add(inputField, c);
 		inputField.setEditable(false);
 
 		// c.add(runButton);
@@ -98,23 +102,76 @@ public class Calculator {
 		for (int i = 0; i < (numberButtons.length); i++) {
 			// Add an event listener to each number key.
 			numberButtons[i].addActionListener(numListener);
-			numberButtons[i].setForeground(numberButtonColor);
+			numberButtons[i].setBackground(numberButtonColor);
+			numberButtons[i].setOpaque(true);
+			// numberButtons[i].setBorderPainted(false);
 
 			// so the focus doesn't get removed from the frame
 			numberButtons[i].setFocusable(false);
 
-			panel.add(numberButtons[i]);
+			// panel.add(numberButtons[i], c);
+
 		}
+
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		int counter = 1;
+
+		// loop through the grid adding numbers 1-9 buttons along the way
+		for (int y = 1; y < 4; y++) {
+			for (int x = 0; x < 3; x++) {
+				c.gridx = x;
+				c.gridy = y;
+
+				panel.add(numberButtons[counter], c);
+				counter++;
+			}
+		}
+
+		// add main operators; + - * /
+		for (int i = 1; i < 5; i++) {
+			c.gridx = 3;
+			c.gridy = i;
+			panel.add(operatorButtons[i - 1], c);
+		}
+
+		// add Clear button
+		c.gridx = 4;
+		c.gridy = 1;
+		panel.add(operatorButtons[4], c);
+
+		// add % button
+		c.gridx = 4;
+		c.gridy = 2;
+		panel.add(operatorButtons[5], c);
+
+		// add = button
+		c.gridy = 3;
+		c.gridheight = 2;
+		panel.add(operatorButtons[6], c);
+
+		// add 0 button
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 2;
+		panel.add(numberButtons[0], c);
+
+		// add . button
+		c.gridx = 2;
+		c.gridy = 4;
+		c.gridwidth = 1;
+		panel.add(numberButtons[10], c);
 
 		for (int i = 0; i < (operatorButtons.length); i++) {
 			// Add an event listener to each operator key.
 			operatorButtons[i].addActionListener(opListener);
 
 			// change the color
-			operatorButtons[i].setForeground(operatorButtonColor);
+			operatorButtons[i].setBackground(operatorButtonColor);
+			operatorButtons[i].setOpaque(true);
 
 			// Add each operator key to the GUI.
-			panel.add(operatorButtons[i]);
+			// panel.add(operatorButtons[i]);
 			operatorButtons[i].setFocusable(false);
 
 		}
