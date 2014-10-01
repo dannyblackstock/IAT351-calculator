5c92bba5c3b26c83d48d96866784e97265b6f071
Danny Blackstock <dannyblackstock@gmail.com>
2014-09-30 20:44:01 -0700

    Clean up the model by removing unnecessary get and set functions.

--- IAT351 Calculator/src/CalculatorModel.java
+++ IAT351 Calculator/src/CalculatorModel.java
@@ -13,33 +13,9 @@ public class CalculatorModel extends Observable {
 	CalculatorModel() {
 	}
 
-	private void setPreviousNumber(double number) {
-		previousNumber = number;
-	}
-
-	private double getPreviousNumber() {
-		return previousNumber;
-	}
-
-	private String getPreviousOperator() {
-		return previousOperator;
-	}
-
-	private void setPreviousOperator(String operator) {
-		previousOperator = operator;
-	}
-	
-	private boolean getStartNewNumber() {
-		return startNewNumber;
-	}
-
 	// variable used to flag whether a new number will be entered
 	private void setStartNewNumber(boolean value) {
 		startNewNumber = value;
-	}
-
-	private boolean getEqualsPressed() {
-		return equalsPressed;
 	}
 
 	// function used to flag whether a the equals button was the last operator
@@ -47,148 +23,6 @@ public class CalculatorModel extends Observable {
 	private void setEqualsPressed(boolean value) {
 		equalsPressed = value;
 	}
-
-	private String getCurrentValue() {
-		return currentNumberStr;
-	}
-	
-	public void doNumber(String numStr) {
-		// if a new number is being entered (ie. after an operator was hit)
-		// first clear the input field
-		if (getStartNewNumber()) {
-			setCurrentValue("");
-		}
-
-		// after the first number was clicked, turn off the flag so that the
-		// numbers clicked afterwards will just be appended
-		setStartNewNumber(false);
-
-		// get current string from the input
-		String currentValue = getCurrentValue();
-
-		// if the number entered was a decimal point
-		if (numStr == ".") {
-			// if the user entered a decimal point first, add a zero in front
-			if (currentValue.isEmpty()) {
-				currentValue = "0.";
-			}
-			// otherwise, if the current number doesn't already have a decimal,
-			// it's OK to add one
-			else if (!currentValue.contains(".")) {
-				// append the clicked button's text to the input field's text
-				currentValue = currentValue + numStr;
-			}
-		} else {
-			// append the clicked button's text to the input field's text
-			currentValue = currentValue + numStr;
-		}
-
-		// change the text
-		setCurrentValue(currentValue);
-
-	}
-	
-	
-	public void doOperation(String operator) {
-	
-		if (operator == "C") {
-			reset();
-			return;
-		}
-		
-		// if equals was pressed before the current operator, but this operator
-		// isn't equals
-		if (getEqualsPressed() && operator != "=") {
-			// set the previous operator to the new (current) one, and prepare
-			// model for a new number to be entered
-			setPreviousOperator(operator);
-
-			// try setting the previous number to the currentValue
-			if (!getCurrentValue().isEmpty()) {
-				setPreviousNumber(Double.parseDouble(getCurrentValue()));
-			} else {
-				setPreviousNumber(0.0);
-			}
-
-			setStartNewNumber(true);
-			setEqualsPressed(false);
-			return;
-		}
-
-		// save temporary variables with the model's values
-		String previousOperator = getPreviousOperator();
-		String currentValue = getCurrentValue();
-		double previousNumber = getPreviousNumber();
-
-		// if equals was the last pressed operator, and the user pressed equals
-		// again
-		if (getEqualsPressed() && operator == "=") {
-			// switch the current number with the previous number so that they
-			// are calculated by the model's functions properly
-			// since the model always does previousNumber then (operator) then
-			// currentNumber
-			setCurrentValue(Double.toString(previousNumber));
-			setPreviousNumber(Double.parseDouble(currentValue));
-		}
-
-		// if there is a previous operator stored
-		if (previousOperator != null) {
-
-			// complete the operation with the pattern
-			// previousNumber then (operator) then currentNumber
-			// see CalculatorModel
-			if (previousOperator == "+") {
-				add();
-			} else if (previousOperator == "-") {
-				subtract();
-			} else if (previousOperator == "*") {
-				multiply();
-			} else if (previousOperator == "/") {
-				divide();
-			} else if (previousOperator == "%") {
-				modulus();
-			}
-		}
-
-		// if the last operator was not equals and the current one isn't either
-		if (!getEqualsPressed() && operator != "=") {
-
-			// try to set the previous number to the current value on the screen
-			if (!getCurrentValue().isEmpty()) {
-				setPreviousNumber(Double.parseDouble(getCurrentValue()));
-			} else {
-				setPreviousNumber(0.0);
-			}
-
-			// save this operator as the previous operator
-			setPreviousOperator(operator);
-			// equals was not the last operator in this case
-			setEqualsPressed(false);
-		}
-
-		// if this is the first time in a row equals has been pressed
-		else if (!getEqualsPressed() && operator == "=") {
-			// save the locally saved variable that was on the screen before as
-			// the previous number so that it can be repeated if the user
-			// presses equals repeatedly
-			setPreviousNumber(Double.parseDouble(currentValue));
-
-			// equals was pressed now
-			setEqualsPressed(true);
-		}
-
-		// if equals was clicked twice in a row
-		else if (getEqualsPressed() && operator == "=") {
-			// change the previousNumber to the temporary saved version of it,
-			// so it can be used again
-			setPreviousNumber(previousNumber);
-		}
-
-		// a new number will need to be entered, so turn on the flag
-		setStartNewNumber(true);
-	}
-	
-	
 
 	private void setCurrentValue(String value) {
 		currentNumberStr = value;
@@ -247,12 +81,148 @@ public class CalculatorModel extends Observable {
 
 	// for the clear button
 	private void reset() {
-		setPreviousOperator(null);
+		previousOperator = null;
+		previousNumber = 0.0;
+		equalsPressed = false;
+
 		setCurrentValue("");
-		setPreviousNumber(0.0);
-		setEqualsPressed(false);
 
 		// a new number will need to be entered, so turn on the flag
-		setStartNewNumber(true);
+		startNewNumber = true;
 	}
+
+	public void doNumber(String numStr) {
+		// if a new number is being entered (ie. after an operator was hit)
+		// first clear the input field
+		if (startNewNumber) {
+			setCurrentValue("");
+		}
+
+		// after the first number was clicked, turn off the flag so that the
+		// numbers clicked afterwards will just be appended
+		startNewNumber = false;
+
+		// get current string from the input
+		String currentValueTemp = currentNumberStr;
+
+		// if the number entered was a decimal point
+		if (numStr == ".") {
+			// if the user entered a decimal point first, add a zero in front
+			if (currentValueTemp.isEmpty()) {
+				currentValueTemp = "0.";
+			}
+			// otherwise, if the current number doesn't already have a decimal,
+			// it's OK to add one
+			else if (!currentValueTemp.contains(".")) {
+				// append the clicked button's text to the input field's text
+				currentValueTemp = currentValueTemp + numStr;
+			}
+		} else {
+			// append the clicked button's text to the input field's text
+			currentValueTemp = currentValueTemp + numStr;
+		}
+
+		// change the text
+		setCurrentValue(currentValueTemp);
+
+	}
+
+	public void doOperation(String operator) {
+
+		if (operator == "C") {
+			reset();
+			return;
+		}
+
+		// if equals was pressed before the current operator, but this operator
+		// isn't equals
+		if (equalsPressed && operator != "=") {
+			// set the previous operator to the new (current) one, and prepare
+			// model for a new number to be entered
+			previousOperator = operator;
+
+			// try setting the previous number to the currentValue
+			if (!currentNumberStr.isEmpty()) {
+				previousNumber = Double.parseDouble(currentNumberStr);
+			} else {
+				previousNumber = 0.0;
+			}
+
+			setStartNewNumber(true);
+			setEqualsPressed(false);
+			return;
+		}
+
+		// save temporary variables with the model's values
+		String currentValueTemp = currentNumberStr;
+		double previousNumberTemp = previousNumber;
+
+		// if equals was the last pressed operator, and the user pressed equals
+		// again
+		if (equalsPressed && operator == "=") {
+			// switch the current number with the previous number so that they
+			// are calculated by the model's functions properly
+			// since the model always does previousNumber then (operator) then
+			// currentNumber
+			setCurrentValue(Double.toString(previousNumberTemp));
+			previousNumber = Double.parseDouble(currentValueTemp);
+		}
+
+		// if there is a previous operator stored
+		if (previousOperator != null) {
+
+			// complete the operation with the pattern
+			// previousNumber then (operator) then currentNumber
+			// see CalculatorModel
+			if (previousOperator == "+") {
+				add();
+			} else if (previousOperator == "-") {
+				subtract();
+			} else if (previousOperator == "*") {
+				multiply();
+			} else if (previousOperator == "/") {
+				divide();
+			} else if (previousOperator == "%") {
+				modulus();
+			}
+		}
+
+		// if the last operator was not equals and the current one isn't either
+		if (!equalsPressed && operator != "=") {
+
+			// try to set the previous number to the current value on the screen
+			if (!currentNumberStr.isEmpty()) {
+				previousNumber = (Double.parseDouble(currentNumberStr));
+			} else {
+				previousNumber = (0.0);
+			}
+
+			// save this operator as the previous operator
+			previousOperator = operator;
+			// equals was not the last operator in this case
+			equalsPressed = false;
+		}
+
+		// if this is the first time in a row equals has been pressed
+		else if (!equalsPressed && operator == "=") {
+			// save the locally saved variable that was on the screen before as
+			// the previous number so that it can be repeated if the user
+			// presses equals repeatedly
+			previousNumber = Double.parseDouble(currentValueTemp);
+
+			// equals was pressed now
+			equalsPressed = true;
+		}
+
+		// if equals was clicked twice in a row
+		else if (equalsPressed && operator == "=") {
+			// change the previousNumber to the temporary saved version of it,
+			// so it can be used again
+			previousNumber = previousNumberTemp;
+		}
+
+		// a new number will need to be entered, so turn on the flag
+		startNewNumber = true;
+	}
+
 }
